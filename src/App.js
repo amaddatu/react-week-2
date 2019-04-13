5cbffd3f6cf5ee77df95a57a1e9890c2d77bf0b3
Anthony Maddatu <amaddatu@gmail.com>
2019-04-13 13:14:25 -0400

    Add all routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Discover from './components/Discover';
+import Search from './components/Search';
+import Nav from './components/Nav';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Route exact path="/" render={
+          props => (
+            <Fragment>
+              <Nav {...props} />
+              <Home {...props} />
+            </Fragment>
+          )
+        } />
+        <Route exact path="/about" render={
+          props => (
+            <Fragment>
+              <Nav {...props} />
+              <About {...props} />
+            </Fragment>
+          )
+        } />
+        <Route exact path="/discover" render={
+          props => (
+            <Fragment>
+              <Nav {...props} />
+              <Discover {...props} />
+            </Fragment>
+          )
+        } />
+        <Route exact path="/search" render={
+          props => (
+            <Fragment>
+              <Nav {...props} />
+              <Search {...props} />
+            </Fragment>
+          )
+        } />
+      </Router>
     );
   }
 }
